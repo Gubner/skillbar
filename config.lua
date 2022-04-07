41f68ed5c28cb5e55414abc1f4622e264df2d49f
Gubner <90917279+Gubner@users.noreply.github.com>
2022-04-07 15:21:14 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,42 @@
+Config = {}
+Config.SkillbarX = 0.50
+Config.SkillbarY = 0.90
+Config.SkillbarHeight = 0.040
+Config.SkillbarBorderColor = {
+	['R'] = 255,
+	['G'] = 255,
+	['B'] = 255,
+	['A'] = 255
+}
+Config.SkillbarColor = {
+	['R'] = 0,
+	['G'] = 0,
+	['B'] = 0,
+	['A'] = 200
+}
+Config.TargetColor = {
+	['R'] = 200,
+	['G'] = 200,
+	['B'] = 200,
+	['A'] = 200
+}
+Config.TriggerOutColor = {
+	['R'] = 255,
+	['G'] = 0,
+	['B'] = 0,
+	['A'] = 255
+}
+Config.TriggerInColor = {
+	['R'] = 0,
+	['G'] = 255,
+	['B'] = 0,
+	['A'] = 255
+}
+Config.MessageColor = {
+	['R'] = 255,
+	['G'] = 255,
+	['B'] = 255,
+	['A'] = 255
+}
+Config.TraverseCount = 2 -- bounces (keep low to prevent convergence)
+Config.DisplayResultDuration = 1000
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gubner'
+description 'Skill Check Script'
+version '1.0.0'
+
+client_script 'client/client.lua'
+
+shared_script 'config.lua'
+
+exports {
+    'GetSkillbarObject'
+}
+
+dependencies {
+    'qb-core'
+}
+
+lua54 'yes'
